d63da35d78814b0fe05d40924e0cf5585cac9f2b
yuri costa <yuri342costa@gmail.com>
2020-11-19 22:40:47 -0300

    arrays

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("qual tamanho da lista que voce quer?");
+            int tamanho = int.Parse(Console.ReadLine());
+            Console.Foreground = ConsoleColor.Green;
+
+            string[] nomesalunos = new string[tamanho];
+            
+
+            for(int i = 0 < nomesalunos.Length; i++){
+                Console.Write("digite um nome :");
+                nomesalunos[i] = Console.ReadLine();
+
+            }
+
+             for(var i = 0 < nomesalunos.Length; i++){
+                  Console.WriteLine(nomesalunos[i]);
+             }
+
+
+            
+            
+        }
+    }
+}
